b397f68fe2fc882c87235ee33120f5ebb9f89e52
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-03-21 03:42:12 +0000

    init

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">AArea aof triangle, in PHP</span>
+        <span class="mdl-layout-title">Area aof triangle, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
